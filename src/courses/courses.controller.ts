e911fcf854e9102aaefa35e5d2ce3050c1946a8c
Jonathan Morales <blonder413@gmail.com>
2024-12-23 17:08:43 -0500

    excepciones

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,11 +1,14 @@
 import {
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
   Post,
   Body,
   Patch,
   Param,
   Delete,
+  HttpException,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
@@ -18,10 +21,19 @@ export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createCourseDto: CreateCourseDto) {
     console.log('__DB__', process.env.DB_NAME);
 
-    return this.coursesService.create(createCourseDto);
+    try {
+      const { price } = createCourseDto;
+      if (price == 413) {
+        throw new HttpException('El precio es muy pro', HttpStatus.BAD_REQUEST);
+      }
+      return this.coursesService.create(createCourseDto);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
   @Get()
